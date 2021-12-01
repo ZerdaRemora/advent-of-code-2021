293313047cd91e2982a774ec5b8e1580e572529d
ZerdaRemora <james@zerdaremora.tech>
2021-12-01 13:25:02 +0000

    Day 1 Solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    // Part 1
+    fun countNumberOfIncreases(input: List<String>): Int {
+        var count = 0
+        for (i in 1 until input.size) {
+            if (input[i].toInt() > input[i - 1].toInt()) {
+                count += 1
+            }
+        }
+        return count
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    // Part 2
+    fun countNumberOfSlidingWindowIncreases(input: List<String>): Int {
+        val windowSums = input.windowed(3) { list -> list.sumOf { it.toInt() } }
+        var count = 0
+        for (i in 1 until windowSums.size) {
+            if (windowSums[i] > windowSums[i - 1]) {
+                count += 1
+            }
+        }
+
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(countNumberOfIncreases(testInput) == 7)
+    check(countNumberOfSlidingWindowIncreases(testInput) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(countNumberOfIncreases(input))
+    println(countNumberOfSlidingWindowIncreases(input))
 }
